b5ff6beed37e89e5fcb7ad429956a77d23599341
Rethabile Diale <rethadiale27@gmail.com>
2024-05-15 14:48:58 +0200

    Function to apply search filters to book data is added

--- scripts.js
+++ scripts.js
@@ -90,6 +90,15 @@ const openOverlay = (selector, focusSelector = null) => {
     getElement(selector).open = true;
     if (focusSelector) getElement(focusSelector).focus();
 };
+
+const applySearchFilters = (filters) => {
+    return books.filter((book) => {
+        const titleMatch = filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase());
+        const authorMatch = filters.author === 'any' || book.author === filters.author;
+        const genreMatch = filters.genre === 'any' || book.genres.includes(filters.genre);
+        return titleMatch && authorMatch && genreMatch;
+    });
+};
 --
 const genreHtml = document.createDocumentFragment();
 const firstGenreElement = document.createElement("option");
